409ef8f58a6e08105ae5e8d44375c846af1217fa
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-03-06 14:24:06 -0800

    Add bug fix to delete with no value

--- public/js/validationEditTask.js
+++ public/js/validationEditTask.js
@@ -1,20 +1,27 @@
-document.getElementById("edit-task-form").onsubmit = () => {
+document.getElementById("edit-task-form").onsubmit = (event) => {
   let fields = ["title", "description", "dueDate", "location"];
   let isValid = true;
 
-  fields.forEach((field) => {
-    document.getElementById(`err-${field}`).style.display = "none";
-    document.getElementById(field).classList.remove("form-input-err");
-  });
+  const isDeletePressed =
+    event.submitter && event.submitter.id === "delete-btn";
 
-  fields.forEach((field) => {
-    let input = document.getElementById(field);
-    if (input.value.trim() === "") {
-      document.getElementById(`err-${field}`).style.display = "block";
-      input.classList.add("form-input-err");
-      isValid = false;
-    }
-  });
+  if (!isDeletePressed) {
+    fields.forEach((field) => {
+      document.getElementById(`err-${field}`).style.display = "none";
+      document.getElementById(field).classList.remove("form-input-err");
+    });
+
+    fields.forEach((field) => {
+      let input = document.getElementById(field);
+      if (input.value.trim() === "") {
+        document.getElementById(`err-${field}`).style.display = "block";
+        input.classList.add("form-input-err");
+        isValid = false;
+      }
+    });
+  }
 
   return isValid;
 };
+
+function isDeleteBtn() {}
